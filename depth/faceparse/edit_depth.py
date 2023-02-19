9a244ddad5915b50a1246219cda713eb411d2f24
SShowbiz <hwkim408@gmail.com>
2023-02-19 13:27:03 +0000

    feat: add edges on mouth region

--- depth/faceparse/edit_depth.py
+++ depth/faceparse/edit_depth.py
@@ -18,14 +18,17 @@ parser.add_argument("--output_path", default="results18", type=str, help="direct
 args = parser.parse_args()
 
 VAL = 5
-EG_VAL = 8
+EG_EYE_VAL = 5
+EG_MOUTH_VAL = 2
+EG_HAIR_VAL = 2
 EYE_VAL = 5
 HAIR_VAL = 10
-LIP_VAL = 5
-EYEBROW_VAL=5
+LIP_VAL = 10
+EYEBROW_VAL= 5
+MOUTH_VAL = 5
 NOSE_VAL = 5
 THRESHOLD = 120
-EG_TRHESHOLD = 25
+EG_TRHESHOLD = 200
 
 class ProcessDepthMap:
     def __init__(self, args):
@@ -37,9 +40,13 @@ class ProcessDepthMap:
     
     def scale_contour(self, cnt, scale):
         M = cv2.moments(cnt)
-        cx = int(M['m10']/M['m00'])
-        cy = int(M['m01']/M['m00'])
 
+        if M['m00'] == 0:
+            cx, cy = np.mean(cnt, axis=0)
+        else:
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+        
         cnt_norm = cnt - [cx, cy]
         cnt_scaled = cnt_norm * scale
         cnt_scaled = cnt_scaled + [cx, cy]
@@ -48,7 +55,7 @@ class ProcessDepthMap:
         return cnt_scaled
     
     def vis_parsing_maps(self, im, dm, eg, parsing_anno, stride, save_im=False, save_path='vis_results/parsing_map_on_im.jpg'):        
-        part_colors = [0, 0, EYEBROW_VAL, EYEBROW_VAL, -EYE_VAL, -EYE_VAL, -EYE_VAL, 0, 0, 0, NOSE_VAL, 0, LIP_VAL, LIP_VAL, 0, 0, 0, -HAIR_VAL]
+        part_colors = [0, 0, EYEBROW_VAL, EYEBROW_VAL, -EYE_VAL, -EYE_VAL, -EYE_VAL, 0, 0, 0, NOSE_VAL, -MOUTH_VAL, LIP_VAL, LIP_VAL, 0, 0, 0, -HAIR_VAL]
         atts = ['skin', 'l_brow', 'r_brow', 'l_eye', 'r_eye', 'eye_g', 'l_ear', 'r_ear', 'ear_r',
                     'nose', 'mouth', 'u_lip', 'l_lip', 'neck', 'neck_l', 'cloth', 'hair', 'hat']
         
@@ -62,10 +69,12 @@ class ProcessDepthMap:
 
         eg = eg.convert("L")
         eg = np.array(eg)
-        eg[eg < EG_TRHESHOLD] = 0
-        max_eg = np.max(eg)
-        min_eg = np.min(eg)
-        norm_eg = (eg - min_eg)/(max_eg - min_eg) * EG_VAL
+
+        eg_eye = eg.copy()
+        eg_hair = eg.copy()
+        eg_mouth = eg.copy()
+
+        eg_hair[eg_hair < 150] = 0
 
         vis_parsing_anno = parsing_anno.copy().astype(np.uint8)
         vis_parsing_anno = cv2.resize(vis_parsing_anno, None, fx=stride, fy=stride, interpolation=cv2.INTER_NEAREST)
@@ -81,24 +90,33 @@ class ProcessDepthMap:
 
                 vis_parsing_anno_eye[vis_parsing_anno_eye != idx] = 0
                 vis_parsing_anno_eye[vis_parsing_anno_eye == idx] = 1
-                vis_parsing_anno_eye_contour = np.zeros_like(vis_parsing_anno_eye)
+                # vis_parsing_anno_eye_contour = np.zeros_like(vis_parsing_anno_eye)
+                # vis_parsing_anno_eg_contour = np.zeros_like(vis_parsing_anno_eye)
 
-                contours, _ = cv2.findContours(vis_parsing_anno_eye, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
-                if len(contours) > 0:
-                    selected_contour = contours[0]
-                    for contour in contours:
-                        num, _, __ = contour.shape
-                        if num > selected_contour.shape[0]:
-                            selected_contour = contour
+                kernel = np.ones((3, 3), np.uint8)
+                vis_parsing_anno_eye = cv2.dilate(vis_parsing_anno_eye, kernel, iterations=10)
 
-                    selected_contour = selected_contour.squeeze(1)
-                    try:
-                        vis_parsing_anno_eye_contour = cv2.fillPoly(vis_parsing_anno_eye_contour, pts=[self.scale_contour(selected_contour, 1.5)], color=[255])
-                    except:
-                        pass
+                index = np.where((vis_parsing_anno_eye == 1) & (eg_eye > 0))
+                vis_parsing_anno_color[index[0], index[1]] -= EG_EYE_VAL
 
-                index = np.where(vis_parsing_anno_eye_contour == 255)
-                vis_parsing_anno_color[index[0], index[1]] += part_colors[idx]
+                # contours, _ = cv2.findContours(vis_parsing_anno_eye, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+                # if len(contours) > 0:
+                #     selected_contour = contours[0]
+                #     for contour in contours:
+                #         num, _, __ = contour.shape
+                #         if num > selected_contour.shape[0]:
+                #             selected_contour = contour
+
+                #     selected_contour = selected_contour.squeeze(1)
+                    
+                #     vis_parsing_anno_eg_contour = cv2.fillPoly(vis_parsing_anno_eg_contour, pts=[self.scale_contour(selected_contour, 2.5)], color=[255])
+                #     vis_parsing_anno_eye_contour = cv2.fillPoly(vis_parsing_anno_eye_contour, pts=[self.scale_contour(selected_contour, 1.5)], color=[255])
+                #     index_eg = np.where((vis_parsing_anno_eg_contour == 255) & (eg_eye > 0))
+                #     vis_parsing_anno_color[index_eg[0], index_eg[1]] -= EG_EYE_VAL
+                   
+
+                # index = np.where(vis_parsing_anno_eye_contour == 255)
+                # vis_parsing_anno_color[index[0], index[1]] += part_colors[idx]
 
         
         for region in ['l_eye', 'r_eye']:
@@ -107,7 +125,25 @@ class ProcessDepthMap:
             index = np.where((vis_parsing_anno == idx) & (im_gray < THRESHOLD))
             vis_parsing_anno_color[index[0], index[1]] -= EYE_VAL
 
-        np_vis = np_dm + vis_parsing_anno_color - norm_eg
+        # for region in ['hair']:
+        #     idx = part2idx[region] + 1
+        #     index = np.where((vis_parsing_anno == idx) & (eg_hair > 0))
+        #     vis_parsing_anno_color[index[0], index[1]] -= EG_HAIR_VAL
+        
+        for region in ['mouth', 'u_lip', 'l_lip']:
+            idx = part2idx[region] + 1
+
+            vis_parsing_anno_mouth = np.copy(vis_parsing_anno)
+            vis_parsing_anno_mouth[vis_parsing_anno_mouth != idx] = 0
+            vis_parsing_anno_mouth[vis_parsing_anno_mouth == idx] = 1
+
+            kernel = np.ones((3, 3), np.uint8)
+            vis_parsing_anno_mouth = cv2.dilate(vis_parsing_anno_mouth, kernel, iterations=15)
+
+            index = np.where((vis_parsing_anno_mouth == 1) & (eg_mouth > 0))
+            vis_parsing_anno_color[index[0], index[1]] -= EG_MOUTH_VAL
+
+        np_vis = np_dm + vis_parsing_anno_color
         min_val = np.min(np_vis)
         max_val = np.max(np_vis)
         np_vis = (np_vis - min_val) / (max_val - min_val) * 255.0
